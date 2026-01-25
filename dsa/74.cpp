e19849a200655c0df95b717a402b9b793a17afc4
harshit4456 <rharshit220@gmail.com>
2026-01-25 12:48:35 +0530

    Create 74.cpp

---
+++ dsa/74.cpp
@@ -0,0 +1,28 @@
+//search in 2d array
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int st=0;
+        int end=row*col-1;
+        int mid=st+(end-st)/2;
+        while(st<=end){
+            
+            int element=matrix[mid/col][mid%col];
+            if(element==target){
+                return 1;
+            }
+            else if(element<target){
+                st=mid+1;
+
+            }
+            else{
+                end=mid-1;
+            }
+            mid=st+(end-st)/2;
+        }
+         return 0;
+    }
+};
