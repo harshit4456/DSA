96b229b4a8e15304be9b47c5f115e3747fc4a47b
harshit4456 <rharshit220@gmail.com>
2026-01-26 15:31:15 +0530

    Update 9.cpp

--- dsa/9.cpp
+++ dsa/9.cpp
@@ -17,3 +17,20 @@ public:
         return (orgi==rev);
     }
 };
+
+
+
+OR 
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        string s = to_string(x);
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s[i++] != s[j--]) return false;
+        }
+        return true;
+    }
+};
