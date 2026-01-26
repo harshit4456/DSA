6dec1469be5c0ef0d4aa27f1a6c898aa07ec0d81
harshit4456 <rharshit220@gmail.com>
2026-01-26 15:29:23 +0530

    Create 9.cpp

---
+++ dsa/9.cpp
@@ -0,0 +1,19 @@
+// palindrome of number
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+            }
+
+        long long rev=0;
+        int orgi=x;
+        while(x>0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+            }
+        return (orgi==rev);
+    }
+};
